280a980c9a7a815c62a1441b68dccc4a1b5e07f8
discolarrence <discolarrence@gmail.com>
2021-04-11 22:28:39 -0400

    wrote script, added imgs

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-let key = document.querySelectorAll
+function playSound(event) {
+    const sound = document.querySelector(`audio[code="${event.code}"]`);
+    const key = document.querySelector(`.key[code="${event.code}"]`);
+    if(!sound) return;
+    sound.currentTime = 0;
+    sound.play();
+    key.classList.add('playing');
+};
+
+function removeTransition(event) {
+    if (event.propertyName !== 'transform') return;
+    this.classList.remove('playing')
+};
+
+const keys = document.querySelectorAll('.key');
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+window.addEventListener('keydown', playSound);
